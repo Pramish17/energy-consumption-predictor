71774e9c3f4d1419e100d8562b250990c04ca7c3
Pramish Thapa <pramish.thapa17@gmail.com>
2024-09-05 17:50:44 +0100

    heruko code updated

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,16 +3,33 @@ import { Container, Typography, Box } from '@mui/material';
 
 const Home = () => {
   return (
-    <Container maxWidth="lg">
-      <Box mt={5}>
+    <Box
+      sx={{
+        backgroundImage: 'url("/images/background.jpg")', // Make sure the image is in public/images
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+        height: '100vh', // Full height of the viewport
+        width: '100vw', // Full width of the viewport
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+        color: '#fff', // White text for contrast
+        textAlign: 'center',
+        overflow: 'hidden', // Ensures no scrolling
+        padding: '20px',
+        position: 'relative', // Keep the container in the viewable area
+      }}
+    >
+      <Container maxWidth="lg">
         <Typography variant="h3" align="center" gutterBottom>
           Welcome to the Energy Consumption Predictor
         </Typography>
         <Typography variant="h6" align="center">
           Use our tool to predict energy consumption based on different variables.
         </Typography>
-      </Box>
-    </Container>
+      </Container>
+    </Box>
   );
 };
 
